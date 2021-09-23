7db765fc132a4e0e051eba7be2f673c81f4cee17
Andrea Angola <3469@holbertonschool.com>
2021-09-22 20:28:20 -0700

    no_int_error

--- print_error.c
+++ print_error.c
@@ -43,3 +43,14 @@ int unknown_op_error(char *opcode, unsigned int line_number)
 		line_number, opcode);
 	return (EXIT_FAILURE);
 }
+/**
+ * no_int_error - Prints invalid monty_push argument error messages.
+ * @line_number: Line number in Monty bytecodes file where error occurred.
+ *
+ * Return: (EXIT_FAILURE) always.
+ */
+int no_int_error(unsigned int line_number)
+{
+	fprintf(stderr, "L%u: usage: push integer\n", line_number);
+	return (EXIT_FAILURE);
+}
