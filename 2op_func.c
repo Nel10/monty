3ec122b6e9e5a8d828caafb4f2fff3dc95e368bf
Nel10 <3461@holbertonschool.com>
2021-09-22 23:13:19 -0500

    fixed

--- 2op_func.c
+++ 2op_func.c
@@ -3,7 +3,6 @@
  * monty_add - Adds the top two values of a stack_t linked list.
  * @stack: A pointer to the top mode node of a stack_t linked list.
  * @line_number: The current working line number of a Monty bytecodes file.
- *
  * Description: The result is stored in the second value node
  *              from the top and the top value  is removed.
  */
