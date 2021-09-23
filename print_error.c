d893a1c85046146ad100cfd935fc4d0b8ba4b522
Andrea Angola <3469@holbertonschool.com>
2021-09-22 20:16:55 -0700

    malloc error

--- print_error.c
+++ print_error.c
@@ -9,3 +9,13 @@ int usage_error(void)
 	fprintf(stderr, "USAGE: monty file\n");
 	return (EXIT_FAILURE);
 }
+/**
+ * malloc_error - Prints malloc error messages.
+ *
+ * Return: (EXIT_FAILURE) always.
+ */
+int malloc_error(void)
+{
+	fprintf(stderr, "Error: malloc failed\n");
+	return (EXIT_FAILURE);
+}
