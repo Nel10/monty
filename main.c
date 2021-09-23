e025758f028c715f60b35681f19b331ab5b8fc08
Nel10 <3461@holbertonschool.com>
2021-09-22 22:20:55 -0500

    betty

--- main.c
+++ main.c
@@ -1,25 +1,16 @@
 #include "monty.h"
-<<<<<<< HEAD
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-=======
->>>>>>> 1a80f3e35779d3f925cd78297ccb002ece37a9c3
-
-char **op_toks = NULL;
 
 /**
  * main - the entry point for Monty Interp
- *
  * @argc: the count of arguments passed to the program
  * @argv: pointer to an array of char pointers to arguments
- *
  * Return: (EXIT_SUCCESS) on success (EXIT_FAILURE) on error
  */
 int main(int argc, char **argv)
 {
 	FILE *script_fd = NULL;
 	int exit_code = EXIT_SUCCESS;
+	char **op_toks = NULL;
 
 	if (argc != 2)
 		return (usage_error());
